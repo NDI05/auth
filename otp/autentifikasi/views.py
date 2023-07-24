c1762cf6b7f55a5df72178ad30c80f172214ffd1
Eka <macnsalt@gmail.com>
2023-07-24 15:01:37 +0800

    added views

--- otp/autentifikasi/views.py
+++ otp/autentifikasi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from models import UserManager
 
-# Create your views here.
+# def index(request):
+#     return render(request, 'autentifikasi/index.html')
+
+def addUser(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        UserManager.objects.create_user(email, username, password)
+        users = UserManager.objects.all()
+        context = {
+        'users': users,
+        }
+        return render(request, 'autentifikasi/listUsers.html', context)
