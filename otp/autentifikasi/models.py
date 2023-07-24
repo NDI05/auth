15b319c015ec73df1f1f9af63d833c4b95a4ff83
Eka <macnsalt@gmail.com>
2023-07-24 14:56:03 +0800

    added delete user

--- otp/autentifikasi/models.py
+++ otp/autentifikasi/models.py
@@ -17,3 +17,12 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def delete_user(self, email):
+        if not email:
+            raise ValueError("User must have an email address")
+        user = self.model(
+            email = self.normalize_email(email),
+        )
+        user.delete(using=self._db)
+        return user
